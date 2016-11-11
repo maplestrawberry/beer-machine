c107d4df9d7002ad545d8e1392cda88111190405
maplestrawberry <anastasyaionas@gmail.com>
2016-11-11 15:42:10 -0500

    Update beer-sample-code.py

--- python/beer-sample-code.py
+++ python/beer-sample-code.py
@@ -6,11 +6,6 @@ to the Raspberry Pi. Modify accordingly for the Beer Machine project. This shoul
 Original author: Richard Moore (ricmoo). 
 
 '''
-
-
-
-
-
 #from display import lcd
 import json
 import time
@@ -24,30 +19,12 @@ import time
 def setup():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(16, GPIO.OUT)
-	
-
 
 def turn_on():
 	GPIO.output(16, 1)
 	print("output1")
 	time.sleep(14)
       	GPIO.output(16, 0)
-
-
-'''
-GPIO_PIN = 36
-
-def setup():
-     wiringpi.wiringPiSetupGpio()
-     wiringpi.pinMode(GPIO_PIN, 1)
-     wiringpi.digitalWrite(GPIO_PIN, 0)
-
-def turn_on():
-     wiringpi.digitalWrite(GPIO_PIN, 1)
-     time.sleep(3)
-     #wiringpi.delay(int(duration * 1000))
-     wiringpi.digitalWrite(GPIO_PIN, 0)
-'''
 
 API_KEY = '9D13ZE7XSBTJ94N9BNJ2MA33VMAY2YPIRB';
 
@@ -73,37 +50,6 @@ def get_balance(address, testnet = True):
         return int(data['result'])
     except Exception, e:
         raise e
-
-def get_logs(address, topic, from_block = 0, to_block = 'latest', testnet = True):
-    host = "testnet.etherscan.io"
-    if not testnet: host = "etherscan.io"
-    url = (("http://%s/api?module=logs&action=getLogs" % host) +
-           ("&fromBlock=%s" % from_block) +
-           ("&toBlock=%s" % to_block) + 
-           ("&address=%s" % address) +
-           ("&topic0=%s" % topic) +
-           ("&apikey=%s" % API_KEY))
-
-    print url
-
-    ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7'
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-Agent', ua)]
-    try:
-        req = opener.open(url)
-        data = json.loads(req.read())
-        result = [];
-        for item in data['result']:
-            result.append(dict(
-                txid         = str(item['transactionHash']),
-                blockNumber  = int(item['blockNumber'][2:], 16),
-                data         = item['data'],
-                address      = item['address'],
-            ))
-        return result
-    except Exception, e:
-        raise e
-
 
 ADDRESS  = '0x02172f0F61361b6a8Ddd3baf729f870BEB628E51'
 TESTNET  = True
